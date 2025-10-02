122f7754323658ed4644b92b17107f2d6c4048a8
ralphishimwe <r.ishimwe3@alustudent.com>
2025-10-02 18:33:00 +0200

    done

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,10 +12,27 @@ class test_basemodel(unittest.TestCase):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
+        """Instantiate a new model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ("created_at", "updated_at") and isinstance(value, str):
+                    # parse string to datetime
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            # Defaults if missing
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            # New object, no kwargs
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def setUp(self):
         """ """
